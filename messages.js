c1a7d2da3f61b50477e783cff2938679134418d7
marknicholas0607 <95486816+marknicholas0607@users.noreply.github.com>
2021-12-23 23:59:40 +0800

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,250 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".section2", {
+	y: 0,
+	duration: 5,
+	scrollTrigger: {
+		trigger: ".section2",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 5,
+		pin: ".title-text",
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-number", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-number",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 2,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-name", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-name",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-text", {
+	y: -200,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-text",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-number2", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-number2",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 2,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-name2", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-name2",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-text2", {
+	y: -200,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-text2",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-number3", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-number3",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 2,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-name3", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-name3",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-text3", {
+	y: -200,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-text3",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-number4", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-number4",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 2,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-name4", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-name4",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-text4", {
+	y: -200,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-text4",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-number5", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-number5",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 2,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-name5", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-name5",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-text5", {
+	y: -200,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-text5",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-number6", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-number6",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 2,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-name6", {
+	x: -700,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-name6",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
+gsap.to(".message-text6", {
+	y: -200,
+	duration: 3,
+	scrollTrigger: {
+		trigger: ".message-text6",
+		start: "top 100%",
+		end: "center 100%",
+		scrub: 3,
+		toggleActions: "restart pause resume reset",
+		// pin: true, or pin: "class name"
+		markers: false,
+	}
+})
